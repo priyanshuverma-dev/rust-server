b77dc33727c6d85167e90eb616b90df0773ce10a
Priyanshu Verma <priyanshuverma@outlook.in>
2024-08-10 19:07:43 +0530

    Multi-threaded and soft shut down.

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,21 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
-enum Methods {
-    GET,
-    POST,
-    PUT,
-    DELETE,
-}
+use server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind(("127.0.0.1", 80)).unwrap();
 
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -41,7 +42,6 @@ fn handle_connection(mut stream: TcpStream) {
         }
     }
     let status_line = "HTTP/1.1 200 OK";
-
     let contents = "{'hello': 'World'}";
     let length = contents.len();
 
